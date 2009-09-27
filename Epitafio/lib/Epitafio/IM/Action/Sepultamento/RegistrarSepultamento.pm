1857db2c6d5b46082849d8044027a7d4c2e785db
Daniel Ruoso <daniel@ruoso.com>
2009-09-26 21:01:20 -0300

    IM do registro do sepultamento

---
+++ Epitafio/lib/Epitafio/IM/Action/Sepultamento/RegistrarSepultamento.pm
@@ -0,0 +1,41 @@
+package Epitafio::IM::Action::Sepultamento::RegistrarSepultamento;
+use Reaction::Class;
+use namespace::autoclean;
+extends 'Reaction::InterfaceModel::Action';
+use Reaction::Types::Core qw(DateTime);
+
+has model_sepultamento =>
+  ( is => 'ro',
+    metaclass => 'Reaction::Meta::Attribute',
+    handles =>
+    { obitos_validos => 'listar_obitos_em_aberto' },
+  );
+
+has model_obitos =>
+  ( is => 'ro',
+    metaclass => 'Reaction::Meta::Attribute',
+    handles =>
+    { jazigos_validos => 'jazigos_disponiveis' });
+
+has obito =>
+  ( isa => 'DB::Obito', is => 'rw',
+    required => 1, lazy_fail => 1,
+    valid_values => sub { shift->obitos_validos });
+
+has jazigo =>
+  ( isa => 'DB::Jazigo', is => 'rw',
+    required => 1, lazy_fail => 1,
+    valid_values => sub { shift->jazigos_validos });
+
+has vt_reg => (isa => DateTime, is => 'rw', lazy_fail => 1);
+
+sub do_apply  {
+  my ($self) = @_;
+  $self->model_sepultamento->sepultar
+    ({ id_obito => $self->obito->id_obito,
+       id_jazigo => $self->jazigo->id_jazigo,
+       vt_reg => $self->vt_reg });
+}
+
+
+1;
